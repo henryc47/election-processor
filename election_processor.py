7b1d63196bd5b4739f21c91b222dc2d24ef41de6
Henry Chadban <henryj.chadban@gmail.com>
2023-03-16 01:15:07 +1100

    modified election info input as import votes ballot style is now mandatory

--- election_processor.py
+++ election_processor.py
@@ -4,6 +4,11 @@
 import csv
 
 
+#store votes in number preferance form 
+class Vote:
+    def __init__(self,list_votes,quantity=1):
+        self.list_votes = list_votes #where they rank each candidate
+
 
 
 #function to manage the election process
@@ -48,27 +53,13 @@ class Election:
                     else:
                         self.party_list_voting = False
                         print('party list voting disabled')
-                #type of representation for raw votes
-                elif row_number==3:
-                    if extract_data=='number_preference':
-                        self.vote_format = 'number_preference'
-                        #in this format, column represents candidate and number represents order of preference
-                        #this is more similar to how things are displayed on the ballot
-                        #0 represents no preference for that candidate
-                        print('You have selected ', self.vote_format,' voting data')
-                    elif extract_data=='order_preference':
-                        self.vote_format = 'order_preference'
-                        #in this format, number refers to candidates and the order represents order of preference
-                        #this is more memory compact if voters not required to list all preferences
-                        print('You have selected ', self.vote_format,' voting data')
-                        print('WARNING: order preference not fully developed')
                 
                 #how many winners will there be at the election
-                elif row_number==4:
+                elif row_number==3:
                     self.num_winners = extract_data
                     print('There will be ',extract_data, 'winners at the election')
                 #how will non-final ties be eliminated
-                elif row_number==5:
+                elif row_number==4:
                     if extract_data=='same_time':
                         print('Losing candidates will be eliminated at the same time')
                         self.early_tie_handling = 'same_time'
@@ -78,21 +69,21 @@ class Election:
                         print('Losing candidates will be eliminated at the same time')
                         self.early_tie_handling = 'same_time'
                 #minimum number of candidates for a valid vote
-                elif row_number==6:
+                elif row_number==5:
                     self.min_candidates = extract_data
                     if self.min_candidates==0:
                         print("Minimum number of candidates for valid vote : All")
                     else:
                         print("Minimum number of candidates for valid vote : ",self.min_candidates)
                 #maximum number of candidates for a valid vote
-                elif row_number==7:
+                elif row_number==6:
                     self.max_candidates = extract_data
                     if self.max_candidates==0:
                         print("Maximum number of candidates for valid vote : All")
                     else:
                         print("Maximum number of candidates for valid vote : ",self.max_candidates)
                 #how to handle votes where too many candidates named
-                elif row_number==8:
+                elif row_number==7:
                     if extract_data=='discard_extra':
                         self.too_many_candidates_policy = 'discard_extra'
                         print('Extra preferences will be discarded')
@@ -104,7 +95,7 @@ class Election:
                         print("WARNING : ",extract_data,' not a valid method of handling excess preferences')
                         print('Defaulting to Extra preferences will be discarded')
                 #how to handle votes where too few candidates named
-                elif row_number==9:
+                elif row_number==8:
                     if extract_data=='invalid':
                         self.too_few_candidates_policy = 'invalid'
                         print("If too few preferences made, vote is invalid")
@@ -113,7 +104,7 @@ class Election:
                         print("WARNING : ",extract_data,' not a valid method of handling insufficient preferences')
                         print('Defaulting to vote being invalid')
                 #how to handle multiple candidates having the same preference
-                elif row_number==10:
+                elif row_number==9:
                     if extract_data=='discard_from':
                         self.repeat_preferences_policy = 'discard_from'
                         print('If equal preferences made, matching and lower preferences discarded')
@@ -130,7 +121,7 @@ class Election:
                         print('Defaulting to If equal preferences made, matching and lower preferences discarded') 
 
                 #how to handle missed preferences (I.E numbering 1-6, then putting 8)
-                elif row_number==11:
+                elif row_number==10:
                     if extract_data=='discard_from':
                         self.skip_preferences_policy = 'discard_from'
                         print('Disgard preferences made after a skip')
@@ -165,10 +156,9 @@ class Election:
 
     #import raw votes            
     def import_votes(self,votes_csv):
-        self.votes = []
+        self.votes = [] #a list containing all the vote objects
         with open(votes_csv) as csvfile:
             csv_reader = csv.reader(csvfile,delimiter=',')
-            row_number = 0
             for row in csv_reader:
                 self.votes.append(row) #add each candidate to the list of candidates
                 print(row)
